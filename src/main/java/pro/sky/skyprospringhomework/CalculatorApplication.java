3f95e354e653ffaa660468be769e462faffea9fc
astra <diman.lan@Mail.ru>
2022-09-08 18:50:02 +0300

    Second commit

--- src/main/java/pro/sky/skyprospringhomework/CalculatorApplication.java
+++ src/main/java/pro/sky/skyprospringhomework/CalculatorApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SkyproSpringHomeworkApplication {
+public class CalculatorApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SkyproSpringHomeworkApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CalculatorApplication.class, args);
+    }
+
 
 }
