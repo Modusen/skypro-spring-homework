25ca48e3eb004085937d98c925389fea17f77476
astra <diman.lan@Mail.ru>
2022-09-08 17:00:26 +0300

    First commit

---
+++ src/main/java/pro/sky/skyprospringhomework/SkyproSpringHomeworkApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.skyprospringhomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproSpringHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproSpringHomeworkApplication.class, args);
+	}
+
+}
